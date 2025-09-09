b515f9a34795733a4e3ffbd66115e8a99fae988f
Md shakil khan <96341854+shakil-khan666@users.noreply.github.com>
2025-09-09 11:04:55 +0600

    Add files via upload

---
+++ 079__count_string_vowel.py
@@ -0,0 +1,8 @@
+def vowel_positions_only(s):
+    vowels = "aeiouAEIOU"
+    return [i for i, ch in enumerate(s) if ch in vowels]
+
+# টেস্ট করো
+text = "Hello World"
+result = vowel_positions_only(text)
+print("Only vowel positions:", result)
